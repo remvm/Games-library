de50b84e2311c202b1e24c58e565c1c4888b2dc5
remvm <mr.maslennikov.vladimir@yandex.ru>
2024-01-16 12:51:38 +0500

    Добавил React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store.js'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div></div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
